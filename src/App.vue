251440695891e9a23dedc0628f88b1552afc6fcc
hanfeng zhang <zhf@33.cn>
2020-08-06 16:13:34 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <layout></layout>
+    </div>
+</template>
+
+<script>
+import layout from './views/index.vue'
+export default {
+  name: 'App',
+  components: {
+    layout
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import "@/mixins/gb"
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
